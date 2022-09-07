e9dfa3c0f76fea7273e4d05bdb4a7e2d8a070e65
MrRevengerX <80905394+MrRevengerX@users.noreply.github.com>
2022-09-07 23:13:38 +0530

    Style reset added

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./scss/global.scss"; //This is style reset scss
 
 function App() {
   return (
---
+++ src/pages/MeetDevs.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function MeetDevs() {
+  return <div>MeetDevs</div>;
+}
+
+export default MeetDevs;

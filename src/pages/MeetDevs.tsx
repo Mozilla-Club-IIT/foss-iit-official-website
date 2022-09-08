663878c35608cbabd6011217309d3c91ef006b9c
MrRevengerX <80905394+MrRevengerX@users.noreply.github.com>
2022-09-08 18:58:48 +0530

    Hero section structure and basic styling finished.

--- src/pages/MeetDevs.tsx
+++ src/pages/MeetDevs.tsx
@@ -1,7 +1,8 @@
 import React from "react";
+import HeroSection from "../components/devs/HeroSection";
 
 function MeetDevs() {
-  return <div>MeetDevs</div>;
+  return <HeroSection />;
 }
 
 export default MeetDevs;

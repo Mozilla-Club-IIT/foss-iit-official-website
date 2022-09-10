248df923374596ff04a74940f14ea4fc0fb92e58
DevmalWije <devmalwije1@gmail.com>
2022-09-10 20:35:51 +0530

    Applied background decorations to global scss and added member cards to about us section

--- src/pages/AboutUs.tsx
+++ src/pages/AboutUs.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Hero from "../components/about/Hero";
 import Footer from "../components/Footer";
+import TheTeam from "../components/devs/TheTeam";
 
 function AboutUs() {
   return (
     <div>
       <Hero />
+      <TheTeam />
       <Footer />
     </div>
   );

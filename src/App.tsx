8afe67cb13f387bbb206c25b97ed0049f8e5b111
MrRevengerX <80905394+MrRevengerX@users.noreply.github.com>
2022-09-11 20:55:05 +0530

    Fixed alignment for person detail card

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,25 @@
-
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Homepage from './pages/Homepage';
-import AboutUs from './pages/AboutUs';
-import ContactUs from './pages/ContactUs';
-import MeetDevs from './pages/MeetDevs';
-
+import React from "react";
+import logo from "./logo.svg";
+// import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Homepage from "./pages/Homepage";
+import AboutUs from "./pages/AboutUs";
+import ContactUs from "./pages/ContactUs";
+import MeetDevs from "./pages/MeetDevs";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<Homepage />} />
-          <Route path='/about' element={<AboutUs />} />
-          <Route path='/contact' element={<ContactUs />} />
-          <Route path='/devs' element={<MeetDevs />} />
+          <Route path="/" element={<Homepage />} />
+          <Route path="/about" element={<AboutUs />} />
+          <Route path="/contact" element={<ContactUs />} />
+          <Route path="/devs" element={<MeetDevs />} />
         </Routes>
       </BrowserRouter>
     </div>
   );
-
 }
 
 export default App;

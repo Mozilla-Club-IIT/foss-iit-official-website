e9d2fb3cc5c5ca449fb3f724cafdbf4b803e6bfc
MrRevengerX <80905394+MrRevengerX@users.noreply.github.com>
2022-09-11 21:35:14 +0530

    Revert "Delete unwanted directory"
    This reverts commit 39a7d3f263a0f4f3943eed50264a41b6a5993041.

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Homepage from './pages/Homepage';
-import AboutUs from './pages/AboutUs';
-import ContactUs from './pages/ContactUs';
-import MeetDevs from './pages/MeetDevs';
-import Navbar from './Components/Navbar';
-
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Homepage from "./pages/Homepage";
+import AboutUs from "./pages/AboutUs";
+import ContactUs from "./pages/ContactUs";
+import MeetDevs from "./pages/MeetDevs";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-      <Navbar />
+        <Navbar />
         <Routes>
           <Route path="/" element={<Homepage />} />
           <Route path="/about" element={<AboutUs />} />
--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,20 +1,18 @@
-import React from 'react'
-import Hero from '../Components/Home/Hero'
-import Mission from '../Components/Home/Mission'
-import Vision from '../Components/Home/Vision'
-import WhoWeAre from '../Components/Home/WhoWeAre'
+import React from "react";
+import Hero from "../components/Home/Hero";
+import Mission from "../components/Home/Mission";
+import Vision from "../components/Home/Vision";
+import WhoWeAre from "../components/Home/WhoWeAre";
 
 const Homepage = () => {
   return (
     <div>
-        <Hero />
-        <WhoWeAre />
-        <Mission />
-        <Vision />
-
+      <Hero />
+      <WhoWeAre />
+      <Mission />
+      <Vision />
     </div>
-  )
-}
+  );
+};
 
 export default Homepage;
-

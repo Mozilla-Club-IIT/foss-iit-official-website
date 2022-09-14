a8318fa9461b235f66f16c3e3521fd2fdd9ac0ac
AbrarSL <abrarsl2002@gmail.com>
2022-09-15 02:02:40 +0530

    Rewrite to workaround some chromium bugs

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -10,20 +10,15 @@ type propType = {
   children: JSX.Element[]
 };
 
-// An array of event/blog objects will be passed into the carousel as children
 function Carousel(props: propType) {
-  const [currentIndex, setCurrentIndex] = useState(0);
-  const [slides, setSlides] = useState<JSX.Element[]>([]);
+  const [currentSlide, setCurrentSlide] = useState(0);
+  const cardContainerRef = useRef<HTMLDivElement | null>(null);
 
-  const slidesRef = useRef<HTMLDivElement[]>([]);
-
-  // What type is this supposed to be?
   const prevSlide = (e: MouseEvent) => {
     e.preventDefault();
 
-    setCurrentIndex((prevState) => {
+    setCurrentSlide((prevState) => {
       const slideNum = (prevState === 0) ? (props.children.length - 1) : (prevState - 1);
-      scrollToSlide(slideNum);
       return slideNum;
     });
   }
@@ -31,46 +26,33 @@ function Carousel(props: propType) {
   const nextSlide = (e: MouseEvent) => {
     e.preventDefault();
 
-    setCurrentIndex((prevState) => {
+    setCurrentSlide((prevState) => {
       const slideNum = (prevState === (props.children.length - 1)) ? 0 : (prevState + 1);
-      scrollToSlide(slideNum);
       return slideNum;
     });
   }
 
-  function scrollToSlide(slideNum: number) {
-    slidesRef.current[slideNum].scrollIntoView({
-      behavior: "smooth",
-      block: "center",
-      inline: "center"
-    });
-  }
-
   useEffect(() => {
-    // Wrap children inside slides
-    setSlides(props.children.map((item: JSX.Element, index: number) => {
-      return (
-        <div
-          key={index}
-          ref={(ref) => {
-            if (ref !== null) {
-              slidesRef.current.push(ref);
-            }
-          }}
-        >
-          {item}
-        </div>
-      );
-    }));
-  }, [props.children]);
+    if (cardContainerRef.current) {
+      const slideLength = Math.round((cardContainerRef.current.scrollWidth / props.children.length));
+      const slideLeft = slideLength * currentSlide;
+      cardContainerRef.current.scrollTo({ top: 0, left: slideLeft, behavior: "smooth" });
+    }
+  }, [currentSlide, props.children]);
 
   return (
     <div className={Styles.carousel}>
       <button className={Styles.carouselButton} onClick={prevSlide}>
         <BsArrowLeft />
       </button>
-      <div className={Styles.cardContainer}>
-        {slides}
+      <div
+        ref={(ref) => {
+          if (ref) {
+            cardContainerRef.current = ref;
+          }
+        }}
+        className={Styles.cardContainer}>
+        {props.children}
       </div>
       <button className={Styles.carouselButton} onClick={nextSlide}>
         <BsArrowRight />

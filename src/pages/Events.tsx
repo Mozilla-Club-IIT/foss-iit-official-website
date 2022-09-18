3b8683a8bdb2b7bf5af6b9a7cd5c652fb666a3f4
MrRevengerX <80905394+MrRevengerX@users.noreply.github.com>
2022-09-18 17:17:18 +0530

    Events page hero section finished.

---
+++ src/pages/Events.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import EventHero from "../components/events/EventHero";
+
+function Events() {
+  return (
+    <>
+      <EventHero />
+    </>
+  );
+}
+
+export default Events;

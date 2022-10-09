151f6f8262cdd53563a84d25abfba5ff4314b835
chamindujs <chamindujs@gmail.com>
2022-10-09 20:19:15 +0530

    Finish the basic design

--- src/components/events/allEvents/TimelineCard.tsx
+++ src/components/events/allEvents/TimelineCard.tsx
@@ -2,15 +2,15 @@ import React from "react";
 import Styles from "../../../scss/events/AllEvents/EventCard.module.scss";
 import Carousel from "../../Carousel";
 import data from "../../../assets/data/upcomingEventsData";
-import EventCarousel from "../eventCarousel";
+import EventCard from "../allEvents/EventCard";
 
 function Timelinkcard() {
   return (
-    <div className={Styles.container}>
+    <div className={Styles.mainWrapper}>
       
         {data.map((data) => (
-        <div className={Styles.container}>
-        <EventCarousel
+        <div className={Styles.mainContainer}>
+        <EventCard
             key={data.id}
             name={data.name}
             image={data.image}

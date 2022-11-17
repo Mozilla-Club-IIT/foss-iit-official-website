fa167428539cc26c54ebb7100c976dc49e638594
MrRevengerX <80905394+MrRevengerX@users.noreply.github.com>
2022-11-17 16:05:45 +0530

    Update EventHero.tsx

--- src/components/events/EventHero.tsx
+++ src/components/events/EventHero.tsx
@@ -4,17 +4,6 @@ import heroImg1 from "../../assets/svg/event-hero-image1.svg";
 import heroImg2 from "../../assets/svg/event-hero-image2.svg";
 
 function EventHero() {
-  //scroll to upcoming events section
-  // const ctaClicked = () => {
-  //   window.scrollTo({ top: 700, left: 0, behavior: "smooth" });
-  // };
-  const ctaClicked = () => {
-    const element = document.getElementById("UpcomingEvents");
-    if (element) {
-      // 👇 Will scroll smoothly to the top of the next section
-      element.scrollIntoView({ behavior: "smooth" });
-    }
-  };
   return (
     <div className={Styles.container}>
       <h1>EVENTS</h1>

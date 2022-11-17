b6d4d209b9c705a76a1d532d0cea8f65e629407e
MrRevengerX <80905394+MrRevengerX@users.noreply.github.com>
2022-11-17 16:07:55 +0530

    Update EventHero.tsx

--- src/components/events/EventHero.tsx
+++ src/components/events/EventHero.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Styles from "../../scss/events/eventHero.module.scss";
 import heroImg1 from "../../assets/svg/event-hero-image1.svg";
 import heroImg2 from "../../assets/svg/event-hero-image2.svg";
+import { Link } from "react-router-dom";
 
 function EventHero() {
   return (
@@ -10,13 +11,14 @@ function EventHero() {
 
       <img src={heroImg1} alt="" className={Styles.img1} />
       <div className={Styles.elementContainer}>
-
-          <div className={Styles.arrow}>
-            <span></span>
-            <span></span>
-            <span></span>
-          </div>
-        </a>
+        <Link onClick={ctaClicked} to="/all-events">
+          <button className={Styles.eventBtn}>All events</button>
+        </Link>
+        <div className={Styles.arrow}>
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
       </div>
       <img src={heroImg2} alt="" className={Styles.img2} />
     </div>
